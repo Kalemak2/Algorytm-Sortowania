d9713979655f0037d7f39c324ef70a9b67cfb5a6
Kalemak2 <152185330+Kalemak2@users.noreply.github.com>
2024-04-29 23:38:39 +0200

    Update Sort.cs

--- ConsoleApp/Sort.cs
+++ ConsoleApp/Sort.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,18 +7,15 @@ using System.Threading.Tasks;
 namespace ConsoleApp
 {
     class Sort {
-        public static void MaxNumber()
-        {
-            int maxlenght = 10;
-            int n = 0;
-            int[] ints = new int[10];
-            while (n < maxlenght)
-            {
-                Console.Write($"Podaj {n + 1} liczbe:");
-                ints[n] = Convert.ToInt32(Console.ReadLine());
-                n = n + 1;
-            }
 
+        /************************************** 
+        nazwa funkcji:        MaxNumber
+        parametry wejściowe:  ints - tablica liczb całkowitych do posortowania
+        wartość zwracana:     posortowana tablica liczb całkowitych w malejącej kolejności
+        autor:                Kornel Pakulski
+        *************************************/
+        public static int[] MaxNumber(int[] ints)
+        {
             for (int i = 0; i < ints.Length - 1; i++)
             {
                 int max = ints[i];
@@ -33,10 +30,7 @@ namespace ConsoleApp
                 }
                 SelectionSort(ints, maxIndex, i);
             }
-            foreach (var d in ints)
-            {
-                Console.WriteLine(d);
-            }
+            return ints;
         }
         static void SelectionSort(int[] ints, int maxIndex, int i)
         {
